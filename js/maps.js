265ab043fa4d470154d944202eac76543ad64efe
Jesse Horne <j.horne2796@gmail.com>
2015-09-10 04:57:04 -0400

    solved #4; map loads user city,region initially

--- js/maps.js
+++ js/maps.js
@@ -1,5 +1,6 @@
 (function() {
 
+    // Called when user presses Find or the enter key
     function changePosition() {
         if (address.value != "") {
             var newAddress = address.value.split(' ').join('+');
@@ -11,7 +12,7 @@
                     .coordinates
                     .reverse();
                 if (location) {
-                    map.setView(location, 15);
+                    map.setView(location, 11);
                 } else {
                     address.value = "Couldn't find";
                 }
@@ -19,18 +20,30 @@
         }
     }
 
+    // unique config key; you must get this from mapbox
     L.mapbox.accessToken = config.key;
 
     $('#map').height($(window).height() - $('#map').position().top - 10);
 
     var map = L.mapbox.map('map', 'mapbox.streets')
-        .setView([33.923014, -84.406099], 9);
+        .setView([0.0, 0.0], 10);;
 
     var address = document.getElementById('address');
     var button = document.getElementById('find');
 
     var apiCall =
         'https://api.mapbox.com/v4/geocode/mapbox.places/ADDRESS.json?access_token=TOKEN';
+
+    // Get location of user
+    var user_location;
+    $.getJSON('http://ipinfo.io/', function(data) {
+        user_location = data.city + ", " + data.region;
+
+        if (user_location) {
+            address.value = user_location;
+            changePosition();
+        }
+    });
 
     button.onclick = function() {
         changePosition();
